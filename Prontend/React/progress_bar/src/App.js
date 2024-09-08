aa7702c776cb8e0085c363276018e249a712d599
RAJESH KUMAR JHA <rajeshkumarjha007.raj@gmail.com>
2024-09-08 22:51:26 +0530

    Progress Bar

--- Prontend/React/progress_bar/src/App.js
+++ Prontend/React/progress_bar/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ProgressBar from "./components/ProgressBar";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 0.1);
+    }, 20);
+  }, []);
+
   return (
-    <div className="calculator"> 
-      <div className="calculator--header">EMI calculator</div>
+    <div className="app">
+      <span>Progress Bar</span>
+      <ProgressBar value={value} onComplete={() => setSuccess(true)} />
+      <span>{success ? "Complete!" : "Loading..."}</span>
     </div>
   );
 }
-
-export default App;
